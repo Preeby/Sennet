8721877241f3d4b63ff5a15babb3dfa557e8a7e5
Preeby <114959463+Preeby@users.noreply.github.com>
2023-12-31 08:40:45 -0700

    syntax error ;(

--- install.sh
+++ install.sh
@@ -92,7 +92,7 @@ for dep in "${dependencies[@]}"; do
 done
 
 # List of commands to install
-commands=('netdos' 'netpulse' 'sennet)
+commands=('netdos' 'netpulse' 'sennet')
 
 for cmd in "${commands[@]}"; do
     chmod +x Commands/$cmd
